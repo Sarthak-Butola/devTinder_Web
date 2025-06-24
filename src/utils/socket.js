2bcefe67158368fdfdd0b35dd0c6d1b532fc2d65
Sarthak Butola <sarthakbutola@gmail.com>
2025-06-24 16:41:34 +0530

    socket base url change

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
-import { BASE_URL } from "./constants";
+import { BASE_URL, BASE_URL1 } from "./constants";
 
 export const createSocketConnection = ()=>{
-    return io(BASE_URL);
+    return io(BASE_URL1);
 };
